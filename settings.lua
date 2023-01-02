294bd443948c6d6ed48ec5c55709c357802fe1df
Kariton <67470612+Kariton@users.noreply.github.com>
2023-01-02 09:20:37 +0100

    settings for optional logistic pickup and delivery stats

--- settings.lua
+++ settings.lua
@@ -20,4 +20,11 @@ data:extend({
 		default_value = false,
 		allow_blank = false,
 	},
+        {
+                type = "bool-setting",
+                name = "graftorio2-enable-logistic-pickup-and-delivery-stats",
+                setting_type = "runtime-global",
+                default_value = false,
+                allow_blank = false,
+        },
 })
