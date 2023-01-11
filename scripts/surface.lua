84e225ac4a16fb3e47273db9161cb9c6517d1d4a
Kariton <67470612+Kariton@users.noreply.github.com>
2023-01-11 01:23:11 +0100

    add more metrics to 'factorio_surface'

--- scripts/surface.lua
+++ scripts/surface.lua
@@ -13,6 +13,7 @@ local lib = {
             {surface.darkness, "darkness"},
             {surface.wind_speed, "wind_speed"},
             {surface.wind_orientation, "wind_orientation"},
+            {surface.wind_orientation_change, "wind_orientation_change"},
             {surface.dusk, "dusk"},
             {surface.dawn, "dawn"},
             {surface.evening, "evening"},
@@ -21,11 +22,21 @@ local lib = {
 
           for _, stat in pairs(stats) do
             gauges.surface:set(stat[1] or -1, {surface.name, stat[2]})
-            -- peaceful_mode returns bool
-            gauges.surface:set(surface.peaceful_mode and 1 or 0, {surface.name, "peaceful_mode"})
           end
-        end
 
+          -- returns bool
+          local bool_stats = {
+            {surface.peaceful_mode, "peaceful_mode"},
+            {surface.always_day, "always_day"},
+            {surface.freeze_daytime, "freeze_daytime"},
+            {surface.show_clouds, "show_clouds"}
+          }
+
+          for _, stat in pairs(bool_stats) do
+            gauges.surface:set(stat[1] and 1 or 0, {surface.name, stat[2]})
+          end
+
+        end
       end
     end
   }
